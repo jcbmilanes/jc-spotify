c095f8da048774383004210e97910b9679344c7b
JCM <johncm2k8g@gmail.com>
2020-10-16 18:04:17 +0800

    Setup React Context API

---
+++ src/DataLayer.js
@@ -0,0 +1,11 @@
+import React, {createContext, useContext, useReducer} from 'react';
+
+// preparing the data layer
+export const DataLayerContext = createContext();
+
+// actual data layer
+export const DataLayer = ({initialState, reducer, children}) => (
+    <DataLayerContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </DataLayerContext.Provider>
+);
--- src/Player.js
+++ src/Player.js
@@ -4,7 +4,7 @@ import './Player.css';
 function Player() {
     return (
         <div className="player">
-            
+            <h1>Welcome!</h1>
         </div>
     )
 }
