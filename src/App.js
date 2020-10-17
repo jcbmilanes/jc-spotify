f60533d8a132e41713252d86a5c4d8ae8021e0db
JCM <johncm2k8g@gmail.com>
2020-10-17 16:45:47 +0800

    Updated Data layer and reducer then added a dispatch in App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
 
+  /*
   const [token, setToken] = useState(null);
+  */
+
+  // to pull information, this is the sysntax that we need to remember.
+  const [{user, token}, dispatch] = useDataLayerValue();
 
   // Run code based on a given condition
   useEffect(() => {
@@ -19,17 +25,35 @@ function App() {
 
     if(_token)
     {
+      /*
       setToken(_token);
+      */
 
       spotify.setAccessToken(_token);
+    
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      });
 
       spotify.getMe().then(user => {
-        console.log("user >>>", user);
+        //console.log("user >>>", user);
+
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+
       });
     }
 
-    console.log('I HAVE A TOKEN >>>', token);
+    /*console.log('I HAVE A TOKEN >>>', token);*/
+
   }, []);
+
+  // to test if the user and token are being actually stored in the data layer.
+  console.log("user pulled from the data layer >>>", user);
+  console.log("token pulled from the data layer >>>", token);
 
   return (
     <div className="app">
--- src/DataLayer.js
+++ src/DataLayer.js
@@ -9,3 +9,5 @@ export const DataLayer = ({initialState, reducer, children}) => (
         {children}
     </DataLayerContext.Provider>
 );
+
+export const useDataLayerValue = () => useContext(DataLayerContext);
